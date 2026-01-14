fc658bf29de25ff19bc8f2415b8d3663c875a895
Marcus Grönnå <mgronna@gmail.com>
2026-01-14 10:53:29 +0100

    Add project files.

---
+++ PostmanCloneUI/Desktop.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace PostmanCloneUI
+{
+    partial class Desktop
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            // 
+            // Desktop
+            // 
+            AutoScaleDimensions = new SizeF(23F, 57F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1533, 855);
+            Font = new Font("Segoe UI", 18F);
+            Margin = new Padding(6, 6, 6, 6);
+            Name = "Desktop";
+            Text = "Postman Clone by Marcus Grönnå";
+            Load += Form1_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
---
+++ PostmanCloneUI/Desktop.cs
@@ -0,0 +1,15 @@
+namespace PostmanCloneUI
+{
+    public partial class Desktop : Form
+    {
+        public Desktop()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}

062ed1aba3b7052088968b858488457e615a48ab
Marcus Grönnå <mgronna@gmail.com>
2026-01-15 19:46:13 +0100

    add: ApiCalls class with HttpClient
    This enables usage of the method CallApiAsync. It returns a json response as ReadAsStringAsync. If not successfull returns response.StatusCode.

---
+++ PostmanCloneLibrary/ApiCalls.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json.Nodes;
+
+namespace PostmanCloneLibrary;
+
+public class ApiCalls
+{
+    private readonly HttpClient client = new();
+
+    public async Task<string> CallApiAsync(string url)
+    {
+        var response = await client.GetAsync(url);
+
+        if (response.IsSuccessStatusCode) {
+
+            string json = await response.Content.ReadAsStringAsync();
+            return json;
+        }
+        else
+        {
+            return "Bad request" + response.StatusCode;
+        }
+    }
+}

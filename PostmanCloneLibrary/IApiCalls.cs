cfd7026607aa0bcadb395880db8cd6ad5e54142d
Marcus Grönnå <mgronna@gmail.com>
2026-01-19 11:45:28 +0100

    add: Create interface for ApiCalls

---
+++ PostmanCloneLibrary/IApiCalls.cs
@@ -0,0 +1,9 @@
+﻿
+namespace PostmanCloneLibrary
+{
+    public interface IApiCalls
+    {
+        Task<string> CallApiAsync(string url, bool formatOutput = true, HttpAction httpAction = HttpAction.GET);
+        bool IsValidUrl(string url);
+    }
+}

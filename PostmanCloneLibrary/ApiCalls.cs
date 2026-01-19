8ff4162a5bd341c599b7647dac11d0655ec14db7
Marcus Grönnå <mgronna@gmail.com>
2026-01-19 11:49:37 +0100

    refactor: Implement conditional return for CallApiAsync
    Allows choice of pretty json returned or raw json returned.

--- PostmanCloneLibrary/ApiCalls.cs
+++ PostmanCloneLibrary/ApiCalls.cs
@@ -7,18 +7,27 @@ public class ApiCalls : IApiCalls
 {
     private readonly HttpClient client = new();
 
-    public async Task<string> CallApiAsync(string url)
+    public async Task<string> CallApiAsync(string url,
+        bool formatOutput = true,
+        HttpAction httpAction = HttpAction.GET
+        )
     {
         var response = await client.GetAsync(url);
 
-        if (response.IsSuccessStatusCode) {
+        if (response.IsSuccessStatusCode)
+        {
 
             string json = await response.Content.ReadAsStringAsync();
+
+            if (formatOutput)
+            {
             var jsonElement = JsonSerializer.Deserialize<JsonElement>(json);
             string prettyJson = JsonSerializer.Serialize(jsonElement,
-                    new JsonSerializerOptions { WriteIndented = true});
+                        new JsonSerializerOptions { WriteIndented = true });
+                return prettyJson;
+            }
 
-            return prettyJson;
+            return json;
         }
         else
         {

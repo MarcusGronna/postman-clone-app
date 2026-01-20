14ca006c5aa3e33f69871d9fa8fc08906b8077b5
Marcus Grönnå <mgronna@gmail.com>
2026-01-20 17:28:09 +0100

    add: Expand HTTP method support and refactor CallApiAsync
    Refactored ApiCalls and IApiCalls to support additional HTTP methods (PUT, PATCH, DELETE) and more flexible request body handling. Introduced overloaded CallApiAsync methods for both string and HttpContent bodies, improving extensibility and aligning the interface accordingly.

--- PostmanCloneLibrary/ApiCalls.cs
+++ PostmanCloneLibrary/ApiCalls.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Json;
+﻿using System.Text;
+using System.Text.Json;
 
 namespace PostmanCloneLibrary;
 
@@ -6,22 +7,36 @@ public class ApiCalls : IApiCalls
 {
     private readonly HttpClient client = new();
 
-    public async Task<string> CallApiAsync(string url,
-        bool formatOutput = true,
+    public async Task<string> CallApiAsync(
+        string url,
+        string body,
         HttpAction httpAction = HttpAction.GET,
-        string? body = null
+        bool formatOutput = true
+        )
+    {
+        StringContent content = new(body, Encoding.UTF8, "application/json");
+        return await CallApiAsync(url, content, httpAction, true);
+    }
+
+    public async Task<string> CallApiAsync(
+        string url,
+        HttpContent? content = null,
+        HttpAction httpAction = HttpAction.GET,
+        bool formatOutput = true
         )
     {
         var response = httpAction switch
         {
             HttpAction.GET => await client.GetAsync(url),
-            HttpAction.POST => await client.PostAsync(url, new StringContent(body!, System.Text.Encoding.UTF8, "application/json")),
-            _ => throw new NotImplementedException()
+            HttpAction.POST => await client.PostAsync(url, content),
+            HttpAction.PUT => await client.PutAsync(url, content),
+            HttpAction.PATCH => await client.PatchAsync(url, content),
+            HttpAction.DELETE => await client.DeleteAsync(url),
+            _ => throw new ArgumentOutOfRangeException(nameof(httpAction), httpAction, null)
         };
 
         if (response.IsSuccessStatusCode)
         {
-
             string json = await response.Content.ReadAsStringAsync();
 
             if (formatOutput)
--- PostmanCloneLibrary/IApiCalls.cs
+++ PostmanCloneLibrary/IApiCalls.cs
@@ -1,14 +1,11 @@
 ﻿
+
 namespace PostmanCloneLibrary
 {
     public interface IApiCalls
     {
-        Task<string> CallApiAsync(
-            string url, 
-            bool formatOutput = true, 
-            HttpAction httpAction = HttpAction.GET, 
-            string? body = null
-            );
+        Task<string> CallApiAsync(string url, HttpContent? content = null, HttpAction httpAction = HttpAction.GET, bool formatOutput = true);
+        Task<string> CallApiAsync(string url, string body, HttpAction httpAction = HttpAction.GET, bool formatOutput = true);
         bool IsValidUrl(string url);
     }
 }
